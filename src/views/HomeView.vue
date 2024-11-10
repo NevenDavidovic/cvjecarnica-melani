993d341f5a892eac31ee83d9ed116bc75b2864f3
NevenDavidovic <nenoronnie@gmail.com>
2024-11-10 16:05:13 +0100

    adjusting size of hamburger

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,7 +41,7 @@
         <button @click="toggleMobileMenu" class="lg:hidden p-2">
           <svg
             width="43"
-            height="28"
+            height="18"
             viewBox="0 0 43 28"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
